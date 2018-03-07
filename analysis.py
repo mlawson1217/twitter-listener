cc9dc713cf51b5251ea707b4d73f7c063d2c08f0
Matt Lawson <mlawson1217@gmail.com>
2018-03-06 20:58:58 -0500

    quick change

--- analysis.py
+++ analysis.py
@@ -4,6 +4,8 @@ def load_csv(file: str):
     df = pd.read_csv(file, encoding='utf8', delimiter='|')
     return df
 
+def word_counts(ignore: list, data: df):
+    for t in df:
 
 
 if __name__ == "__main__":
