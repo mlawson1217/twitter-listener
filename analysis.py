d96b2a33bc1eb54243f2c8ea9f143ce95bd33e07
Matt Lawson <mlawson1217@gmail.com>
2018-03-06 22:51:02 -0500

    added word count functionality for hashtags

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import subprocess as sub
 import operator
+import ast
 
 def load_csv(file: str):
     df = pd.read_csv(file, encoding='utf8', delimiter='|')
@@ -11,14 +12,14 @@ def load_csv(file: str):
 ignore_words = ["the", "and", "it", "was", "who", "what", "when", "where",
 "why", "for", "how", "your", "a", "to", "more", "[nl]", "of", "from", "with",
 "in", "you", "help", "are", "can", "is", "on", "this", "at", "these", "be",
-"make", "by", "our", "get", "if", "some", "see", "have", "do", "we", "new"]
+"make", "by", "our", "get", "if", "some", "see", "have", "do", "we", "new", "-", "--", "&amp;"]
 
-def word_counts(data):
+def word_counts(data, field):
     count_dict = {}
-    for t in data["text"]:
-        words = t.split()
+    for t in data[field]:
+        words = split_field(t, field)
         for word in words:
-            word = word.lower()
+            word = clean_word(word)
             if word in ignore_words:
                 pass
             else:
@@ -29,8 +30,19 @@ def word_counts(data):
     sorted_dict = sorted(count_dict.items(), key=operator.itemgetter(1))
     return sorted_dict
 
+def split_field(t, field):
+    if field is "text":
+        words = t.split()
+    else:
+        words = ast.literal_eval(t)
+    return words
+
+def clean_word(word):
+    word = word.lower()
+    word = word.replace(":", "")
+    return word
 
 if __name__ == "__main__":
     sub.call(["python", "main.py"])
     csv = load_csv('tweet_output.csv')
-    print(word_counts(csv))
+    print(word_counts(csv, "text"))
